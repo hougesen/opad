5b71891495819dc4146b13c100e5ee91d7097975
Mads Hougesen <mads@mhouge.dk>
2025-05-22 00:11:24 +0200

    feat: add support for pyproject.toml

---
+++ cli/src/package_managers/pyproject.rs
@@ -0,0 +1,31 @@
+pub fn set_version(path: &std::path::Path, version: &str) -> anyhow::Result<bool> {
+    let mut modified = false;
+
+    let contents = std::fs::read_to_string(path)?;
+
+    let mut document = contents.parse::<toml_edit::DocumentMut>()?;
+
+    if let Some(package_raw) = document.get_mut("project") {
+        if let Some(package_table) = package_raw.as_table_like_mut() {
+            let should_modify = package_table
+                .get("version")
+                .is_some_and(|outer| outer.as_str().is_some_and(|inner| inner != version));
+
+            if should_modify {
+                package_table.insert(
+                    "version",
+                    toml_edit::Item::Value(toml_edit::Value::String(toml_edit::Formatted::new(
+                        version.to_string(),
+                    ))),
+                );
+                modified = true;
+            }
+        }
+    }
+
+    if modified {
+        std::fs::write(path, document.to_string())?;
+    }
+
+    Ok(modified)
+}
