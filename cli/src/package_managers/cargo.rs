62c2fabd848e775874676d3c66d046b8cf422c90
Mads Hougesen <mads@mhouge.dk>
2025-05-21 14:09:42 +0200

    feat: add support for cargo workspaces (#2)

--- cli/src/package_managers/cargo.rs
+++ cli/src/package_managers/cargo.rs
@@ -1,3 +1,19 @@
+use toml_edit::TableLike;
+
+fn set_package_version(package_table: &mut dyn TableLike, version: &str) -> bool {
+    if package_table.get("version").is_some_and(|v| v.is_str()) {
+        package_table.insert(
+            "version",
+            toml_edit::Item::Value(toml_edit::Value::String(toml_edit::Formatted::new(
+                version.to_string(),
+            ))),
+        );
+        true
+    } else {
+        false
+    }
+}
+
 pub fn set_cargo_toml_version(path: &std::path::Path, version: &str) -> anyhow::Result<bool> {
     let mut modified = false;
 
@@ -6,15 +22,17 @@ pub fn set_cargo_toml_version(path: &std::path::Path, version: &str) -> anyhow::
     let mut parsed = contents.parse::<toml_edit::DocumentMut>()?;
 
     if let Some(package_raw) = parsed.get_mut("package") {
-        if let Some(package_table) = package_raw.as_table_mut() {
-            if package_table.get("version").is_some_and(|v| v.is_str()) {
-                package_table.insert(
-                    "version",
-                    toml_edit::Item::Value(toml_edit::Value::String(toml_edit::Formatted::new(
-                        version.to_string(),
-                    ))),
-                );
-                modified = true;
+        if let Some(package_table) = package_raw.as_table_like_mut() {
+            modified |= set_package_version(package_table, version);
+        }
+    }
+
+    if let Some(workspace) = parsed.get_mut("workspace") {
+        if let Some(workspace_table) = workspace.as_table_like_mut() {
+            if let Some(package) = workspace_table.get_mut("package") {
+                if let Some(package_table) = package.as_table_like_mut() {
+                    modified |= set_package_version(package_table, version);
+                }
             }
         }
     }
