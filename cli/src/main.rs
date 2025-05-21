52998268f290b092f1653b23c41402ba0782cb6f
Mads Hougesen <mads@mhouge.dk>
2025-05-21 23:07:34 +0200

    feat: command for setting package versions

--- cli/src/main.rs
+++ cli/src/main.rs
@@ -1,32 +1,13 @@
-use std::env::current_dir;
+use crossterm::style::Stylize;
 
-use fs::find_package_manager_files;
-
+mod commands;
 mod fs;
 mod package_managers;
 
-#[inline]
-fn _main() -> anyhow::Result<()> {
-    let files = find_package_manager_files(current_dir()?.as_path());
-
-    let version = "0.0.0";
-
-    let results = files
-        .into_iter()
-        .map(|pm| {
-            let result = pm.set_package_version(version);
-            (pm, result)
-        })
-        .collect::<Vec<_>>();
-
-    println!("results: {results:#?}");
-
-    Ok(())
-}
-
 fn main() {
-    if let Err(error) = _main() {
-        eprintln!("{error}");
-        std::process::exit(0)
+    if let Err(error) = commands::tui::run_command() {
+        eprintln!("{}", format!("{error}").red().bold());
+
+        std::process::exit(1)
     }
 }
--- cli/src/package_managers/mod.rs
+++ cli/src/package_managers/mod.rs
@@ -16,23 +16,70 @@ pub enum PackageManager {
 
     // yarn
     YarnLock,
+
+    // cabal
+    Cabal,
+
+    // stack
+    Stack,
 }
 
 impl PackageManager {
     #[inline]
-    fn maybe_from_file_name(file_name: &str) -> Option<Self> {
-        match file_name {
-            "Cargo.toml" => Some(Self::CargoToml),
-            "Cargo.lock" => Some(Self::CargoLock),
+    fn maybe_from_path(path: &std::path::Path) -> Option<Self> {
+        if let Some(file_name) = path.file_name().and_then(|inner| inner.to_str()) {
+            let pm = match file_name {
+                "Cargo.toml" => Some(Self::CargoToml),
+                "Cargo.lock" => Some(Self::CargoLock),
 
-            "package.json" => Some(Self::PackageJson),
-            "package-lock.json" => Some(Self::PackageLockJson),
+                "package.json" => Some(Self::PackageJson),
+                "package-lock.json" => Some(Self::PackageLockJson),
 
-            "pnpm-lock.yaml" => Some(Self::PnpmLockYaml),
+                "pnpm-lock.yaml" => Some(Self::PnpmLockYaml),
 
-            "yarn.lock" => Some(Self::YarnLock),
+                "yarn.lock" => Some(Self::YarnLock),
 
-            _ => None,
+                "package.yaml" => Some(Self::Stack),
+
+                _ => None,
+            };
+
+            if let Some(pm) = pm {
+                return Some(pm);
+            }
+        }
+
+        if let Some(ext) = path.extension().and_then(|inner| inner.to_str()) {
+            let pm = match ext {
+                "cabal" => Some(Self::Cabal),
+
+                _ => None,
+            };
+
+            if let Some(pm) = pm {
+                return Some(pm);
+            }
+        }
+
+        None
+    }
+
+    #[inline]
+    pub const fn is_enabled(self) -> bool {
+        match self {
+            Self::CargoToml => true,
+            Self::CargoLock => false,
+
+            Self::PackageJson => true,
+            Self::PackageLockJson => false,
+
+            Self::PnpmLockYaml => false,
+
+            Self::YarnLock => false,
+
+            Self::Cabal => false,
+
+            Self::Stack => false,
         }
     }
 }
@@ -44,16 +91,43 @@ pub struct PackageManagerFile {
     pub path: std::path::PathBuf,
 }
 
+impl PartialEq for PackageManagerFile {
+    #[inline]
+    fn eq(&self, other: &Self) -> bool {
+        self.path == other.path
+    }
+}
+
+impl Eq for PackageManagerFile {}
+
+impl PartialOrd for PackageManagerFile {
+    #[inline]
+    fn partial_cmp(&self, other: &Self) -> Option<std::cmp::Ordering> {
+        Some(self.path.cmp(&other.path))
+    }
+}
+
+impl Ord for PackageManagerFile {
+    #[inline]
+    fn cmp(&self, other: &Self) -> std::cmp::Ordering {
+        self.path.cmp(&other.path)
+    }
+}
+
+impl std::fmt::Display for PackageManagerFile {
+    #[inline]
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        write!(f, "{}", self.path.display())
+    }
+}
+
 impl PackageManagerFile {
     #[inline]
     pub fn maybe_from_path(path: &std::path::Path) -> Option<Self> {
-        path.file_name()
-            .and_then(|file_name| file_name.to_str())
-            .and_then(PackageManager::maybe_from_file_name)
-            .map(|package_manager| PackageManagerFile {
-                package_manager,
-                path: path.to_path_buf(),
-            })
+        PackageManager::maybe_from_path(path).map(|package_manager| Self {
+            package_manager,
+            path: path.to_path_buf(),
+        })
     }
 
     #[inline]
@@ -68,6 +142,10 @@ impl PackageManagerFile {
             PackageManager::PnpmLockYaml => Ok(false),
 
             PackageManager::YarnLock => Ok(false),
+
+            PackageManager::Cabal => Ok(false),
+
+            PackageManager::Stack => Ok(false),
         }
     }
 }
