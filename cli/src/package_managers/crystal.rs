42e1957fae4ec7a1c276c4afdfd10d7d9bc8d27f
Mads Hougesen <mads@mhouge.dk>
2025-05-23 20:07:05 +0200

    feat: support crystal shards pm

---
+++ cli/src/package_managers/crystal.rs
@@ -0,0 +1,103 @@
+use crate::parsers::yaml;
+
+#[inline]
+pub fn set_shard_yml_version(path: &std::path::Path, version: &str) -> anyhow::Result<bool> {
+    let mut contents = std::fs::read_to_string(path)?;
+
+    let document = yaml::parse(&contents)?;
+
+    let mut modified = false;
+
+    if let Some(map) = document.as_mapping() {
+        if let Some(version_node) = map.get_node("version") {
+            if let Some(scalar) = version_node.as_scalar() {
+                let output = yaml::replace_node(&contents, scalar, version);
+
+                modified = output != contents;
+
+                contents = output;
+            }
+        }
+    }
+
+    if modified {
+        yaml::save(path, &contents)?;
+    }
+
+    Ok(modified)
+}
+
+#[inline]
+pub const fn update_lock_files(_path: &std::path::Path) -> bool {
+    true
+}
+
+#[cfg(test)]
+mod test_set_shard_yml_version {
+
+    const INPUT: &str = r#"name: crystal-demo
+version:          0.1.0
+
+authors:
+                    - Mads Hougesen <mads@mhouge.dk>
+
+crystal:   ">= 1.15.1"
+
+license:     MIT
+"#;
+
+    #[test]
+    fn it_should_update_version() -> anyhow::Result<()> {
+        let version = "2025.05.23+1722";
+
+        let new_version_line = format!("version:          {version}");
+
+        let expected_output = INPUT.replace("version:          0.1.0", &new_version_line);
+
+        assert!(expected_output.contains(&new_version_line));
+
+        let dir = tempfile::tempdir()?;
+
+        let path = dir.path().join("shard.yml");
+
+        std::fs::write(&path, INPUT)?;
+
+        let modified = super::set_shard_yml_version(&path, version)?;
+
+        assert!(modified);
+
+        let output = std::fs::read_to_string(&path)?;
+
+        assert_eq!(output, expected_output);
+
+        Ok(())
+    }
+
+    #[test]
+    fn it_support_multiline_strings() -> anyhow::Result<()> {
+        let input = INPUT.replace("version:          0.1.0", "version:\n          0.1.0");
+
+        let version = "2025.05.23+1722";
+
+        let new_version_line = format!("version:\n          {version}");
+
+        let expected_output = input.replace("version:\n          0.1.0", &new_version_line);
+
+        assert!(expected_output.contains(&new_version_line));
+
+        let dir = tempfile::tempdir()?;
+
+        let path = dir.path().join("pubspec.yaml");
+
+        std::fs::write(&path, &input)?;
+        let modified = super::set_shard_yml_version(&path, version)?;
+
+        assert!(modified);
+
+        let output = std::fs::read_to_string(&path)?;
+
+        assert_eq!(output, expected_output);
+
+        Ok(())
+    }
+}
