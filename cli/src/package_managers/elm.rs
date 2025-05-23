e8d28ae7f136df8f2c1f5525614f26c0be7bd2ed
Mads Hougesen <mads@mhouge.dk>
2025-05-23 23:04:29 +0200

    feat: add support for updating elm.json

---
+++ cli/src/package_managers/elm.rs
@@ -0,0 +1,12 @@
+use super::npm;
+
+/// `elm.json` follows the same format as `package.json` (?)
+#[inline]
+pub fn set_elm_json_version(path: &std::path::Path, version: &str) -> anyhow::Result<bool> {
+    npm::set_package_json_version(path, version)
+}
+
+#[inline]
+pub const fn update_lock_files(_dir: &std::path::Path) -> bool {
+    true
+}
