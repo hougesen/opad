3d42b0c6ff2d80715aa2f6f81d0c20384efae1a9
Mads Hougesen <mads@mhouge.dk>
2025-05-24 20:38:15 +0200

    fix: disable PackageManagerError::test_up_casting during dev

--- cli/src/package_managers/error.rs
+++ cli/src/package_managers/error.rs
@@ -32,18 +32,21 @@ impl core::fmt::Display for PackageManagerError {
 impl PackageManagerError {
     #[cfg(test)]
     pub fn test_up_casting(self) {
-        let self_to_string = self.to_string();
+        // Disabled during dev to not obscure the coverage
+        if std::env::var("GITHUB_ACTIONS").is_ok() {
+            let self_to_string = self.to_string();
 
-        match &self {
-            Self::ShardYml(inner) => assert_eq!(inner.to_string(), self_to_string),
-            Self::CargoToml(inner) => assert_eq!(inner.to_string(), self_to_string),
-            Self::PackageJson(inner) => assert_eq!(inner.to_string(), self_to_string),
-            Self::GleamToml(inner) => assert_eq!(inner.to_string(), self_to_string),
-            Self::PyprojectToml(inner) => assert_eq!(inner.to_string(), self_to_string),
-            Self::PubspecYaml(inner) => assert_eq!(inner.to_string(), self_to_string),
-        };
+            match &self {
+                Self::ShardYml(inner) => assert_eq!(inner.to_string(), self_to_string),
+                Self::CargoToml(inner) => assert_eq!(inner.to_string(), self_to_string),
+                Self::PackageJson(inner) => assert_eq!(inner.to_string(), self_to_string),
+                Self::GleamToml(inner) => assert_eq!(inner.to_string(), self_to_string),
+                Self::PyprojectToml(inner) => assert_eq!(inner.to_string(), self_to_string),
+                Self::PubspecYaml(inner) => assert_eq!(inner.to_string(), self_to_string),
+            };
 
-        assert_eq!(self_to_string, crate::error::Error::from(self).to_string());
+            assert_eq!(self_to_string, crate::error::Error::from(self).to_string());
+        }
     }
 }
 
